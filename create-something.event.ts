edebed45130bd9a8dccf22e985474b477f658d45
ilesar <ivan.lesar@bornfight.com>
2021-12-10 17:59:50 +0100

    Remove test files

--- create-something.event.ts
+++
@@ -1,7 +0,0 @@
-import { TemplateEvent } from '@core/templates/_template.event';
-
-export class CreateSomethingEvent extends TemplateEvent {
-  constructor() {
-    super({});
-  }
-}
--- something.query.ts
+++
@@ -1,7 +0,0 @@
-import { TemplateQuery } from '@core/templates/_template.query';
-
-export class SomethingQuery extends TemplateQuery {
-  constructor() {
-    super({});
-  }
-}
